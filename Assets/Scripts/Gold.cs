5ae08b3d9302560fffefd8f752169f773c0710fe
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-10-20 12:19:57 +0200

    adds Playerprefs for saving

--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gold : MonoBehaviour {
+
     public int GoldAmount { get; private set; }
     [SerializeField]private int addGold;
 
@@ -10,4 +12,12 @@ public class Gold : MonoBehaviour {
    public void AddGold() {
         GoldAmount += addGold;
     }
+   private void Start() {
+       this.GoldAmount = PlayerPrefs.GetInt("Gold", 1);
+   }
+     
+         private void OnDestroy() {
+             PlayerPrefs.SetInt("Gold", this.GoldAmount);
+         }
+
 }
