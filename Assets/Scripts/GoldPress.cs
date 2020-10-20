6d376419d96d1fd2c96a9a9a18cdf4534139197e
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-10-20 15:28:52 +0200

    adds Gold press to the game.

---
+++ Assets/Scripts/GoldPress.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPress : MonoBehaviour {
+    [SerializeField] private int goldPressOwned;
+    [SerializeField] private int productionCost;
+    [SerializeField] private int goldPerSecondPerPress;
+    private Gold gold;
+
+    void Start() {
+        gold = FindObjectOfType<Gold>();
+        StartCoroutine(StartPrinting());
+    }
+
+    public void BuyGoldPess() {
+        if (gold.GoldAmount > productionCost) {
+            gold.ReduceGold(productionCost);
+            goldPressOwned++;
+        }
+    }
+
+    IEnumerator StartPrinting() {
+        while (true) {
+            yield return new WaitForSeconds(1f);
+            PrintMoney();
+        }
+    }
+
+    void PrintMoney() {
+        var goldPerSecond = goldPerSecondPerPress * goldPressOwned;
+        gold.AddGold(goldPerSecond);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
