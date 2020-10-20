999b4c21ebb5e2c28e75532b8809f59d16fe781f
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-10-20 10:35:15 +0200

    Added Gold + Gold button

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour {
+    public int GoldAmount { get; private set; }
+    [SerializeField]private int addGold;
+
+
+   public void AddGold() {
+        GoldAmount += addGold;
+    }
+}
---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour {
+    public Text goldText;
+    private Gold gold;
+    void Start() {
+        gold = FindObjectOfType<Gold>();
+    }
+
+    void Update() {
+        goldText.text = $"Gold : {gold.GoldAmount}";
+    }
+}
