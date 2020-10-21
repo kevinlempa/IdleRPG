84e82ccdaa42d695e9f05e1611176c2b6e43d845
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-10-21 10:33:29 +0200

    refactors some code

--- Assets/Scripts/GoldPress.cs
+++ Assets/Scripts/GoldPress.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public partial class GoldPress : MonoBehaviour, IPurchasableProduct {
+
     public int goldPressOwned;
     public int productionCost;
     public int goldPerSecondPerPress;
