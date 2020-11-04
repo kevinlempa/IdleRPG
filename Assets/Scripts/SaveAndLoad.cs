de8040a3f792cb768a81ab0544dfa81e6e6e63ba
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-11-04 15:02:56 +0100

    cleanup

--- Assets/Scripts/SaveAndLoad.cs
+++ Assets/Scripts/SaveAndLoad.cs
@@ -10,16 +10,9 @@ public class SaveAndLoad : MonoBehaviour {
     void Start() {
         gold = FindObjectOfType<Gold>();
         gold.GoldAmount = PlayerPrefs.GetInt("Gold", 0);
-        // goldPress = FindObjectOfType<GoldPress>();
-        // goldPress.cost = PlayerPrefs.GetInt("Production Cost", 100);
-        // goldPress.goldPressAmount = PlayerPrefs.GetInt("Gold presses owned", 0);
-        // goldPress.productionAmount = PlayerPrefs.GetInt("Gold per second per press", 1);
     }
 
     private void OnDestroy() {
         PlayerPrefs.SetInt("Gold", gold.GoldAmount);
-        // PlayerPrefs.SetInt("Production Cost", goldPress.cost);
-        // PlayerPrefs.SetInt("Gold presses owned", goldPress.goldPressAmount);
-        // PlayerPrefs.SetInt("Gold per second per press", goldPress.productionAmount);
     }
 }
